eb83e221c2a8199b12a60361f40dd8fd369d9175
yangseok Lee <ysl0715@gmail.com>
2019-04-22 01:22:26 +0900

    .

--- src/index.js
+++ src/index.js
@@ -1,33 +1,22 @@
 const { GraphQLServer } = require('graphql-yoga')
-
-// 런타임 시 링크를 저장하는 데 사용됨. (지금의 모든것은 데이터베이스에 저장되기보다는 메모리에 저장됨.)
-let links = [{
-  id: 'link-0',
-  url: 'www.howtographql.com',
-  description: 'Fullstack tutorial for GraphQL'
-}]
-
-//
-let idCount = links.length
+const { prisma } = require('./generated/prisma-client')
 
 //GraphQL 스키마 실제 구현
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
     // feed 타입에 맞게 데이터를 입력
-    feed: () => links,
+    feed: (root, args, context, info) => {
+      return context.prisma.links()
+    },
   },
   Mutation: {
-    //
-    post: (parent, args) => {
-      const link = {
-        id: `link-${idCount++}`,
+    post: (root, args, context) => {
+      return context.prisma.createLink({
+        url: args.url,
         description: args.description,
-        url: args.url
-      }
-      links.push(link)
-      return link
-    }
+      })
+    },
   },
   // Link 스키마에 정의된 유형에 대해 추가
   Link: {
@@ -40,7 +29,8 @@ const resolvers = {
 //스키마와 리졸버를 GraphQL 서버로 전달. 서버에 허용되는 API 작업과 해결 방법을 서버에 알려줌.
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
-  resolvers
+  resolvers,
+  context: { prisma },
 })
 
 server.start(() => console.log(`Server is running on http://localhost:4000`))
